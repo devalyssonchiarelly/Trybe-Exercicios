5502ebfcd36b8006e3f36edf356ff1e658fea9af
Alysson Chiarelly <alyssonclr@gmail.com>
2022-08-18 18:27:26 -0300

    Adicionado primeiros itens, do último exercicio

---
+++ 01-Fundamentos/Bloco-04-Introducao-a-javascript-e-logica-de-programacao/Dia-1-Javascript-e-primeiros-passos/Exercicios - Final/exercicio-I.js
@@ -0,0 +1,17 @@
+const num1 = 10;
+const num2 = 25;
+
+let adicao = num1 + num2;
+console.log(adicao);
+
+let subtracao = num1 - num2;
+console.log(subtracao);
+
+let multiplicacao = num1 * num2;
+console.log(multiplicacao);
+
+let divisao = num1 / num2;
+console.log(divisao);
+
+let modulo = num1 % num2;
+console.log(modulo);
---
+++ 01-Fundamentos/Bloco-04-Introducao-a-javascript-e-logica-de-programacao/Dia-1-Javascript-e-primeiros-passos/Exercicios - Final/exercicio-II.js
@@ -0,0 +1,8 @@
+const num1 = 10;
+const num2 = 40;
+
+if (num1 > num2) {
+  console.log(num1);
+} else if(num1 < num2) {
+  console.log(num2);
+}
