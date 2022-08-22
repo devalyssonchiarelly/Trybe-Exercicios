587b5f061623cc79ec32bf5be45e7ca5898b20e8
Alysson <alysson@pop-os.localdomain>
2022-08-22 18:31:31 -0300

    Primeiro exercicio da lista final,  adicionado

---
+++ 01-Fundamentos/Bloco-04-Introducao-a-javascript-e-logica-de-programacao/Dia-3-Logica-de-programacao-e-algoritmos/exercicio-1.js
@@ -0,0 +1,9 @@
+//Exercicio calculo de fatorial
+let fatorial = 10;
+
+
+for (let index = 1; index <=10; index+= 1){
+   fatorial = fatorial *= index;
+}
+
+console.log(`O fatorial do número é: ${fatorial}`);
