f9c4e4f15a569589893e0b2eb0032ae26fe023ca
Alysson Chiarelly <alyssonclr@gmail.com>
2022-08-24 09:50:24 -0300

    Organização do exercicio 6 melhorada

--- 01-Fundamentos/Bloco-04-Introducao-a-javascript-e-logica-de-programacao/Dia-4-Objetos-e-Funcoes/Exercicios - Finais/exercicio-6.js
+++ 01-Fundamentos/Bloco-04-Introducao-a-javascript-e-logica-de-programacao/Dia-4-Objetos-e-Funcoes/Exercicios - Finais/exercicio-6.js
@@ -11,4 +11,5 @@ let leitor = {
   ],
 };
 
-console.log('"O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + 'se chama ' +leitor.livrosFavoritos[0].titulo);
+console.log('"O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + 
+'se chama ' +leitor.livrosFavoritos[0].titulo);
