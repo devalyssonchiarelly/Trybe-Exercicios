6cdddf7777db0066ff530bd57659676e60af91dd
Alysson Chiarelly <alyssonclr@gmail.com>
2022-08-24 10:08:10 -0300

    Adicionado exercicio 8

--- 01-Fundamentos/Bloco-04-Introducao-a-javascript-e-logica-de-programacao/Dia-4-Objetos-e-Funcoes/Exercicios - Finais/exercicio-6.js
+++ 01-Fundamentos/Bloco-04-Introducao-a-javascript-e-logica-de-programacao/Dia-4-Objetos-e-Funcoes/Exercicios - Finais/exercicio-6.js
@@ -11,4 +11,4 @@ let leitor = {
   ],
 };
 
-console.log('"O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + 'se chama ' +leitor.livrosFavoritos[0].titulo);
+console.log('"O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + 'se chama ' + leitor.livrosFavoritos[0].titulo);
