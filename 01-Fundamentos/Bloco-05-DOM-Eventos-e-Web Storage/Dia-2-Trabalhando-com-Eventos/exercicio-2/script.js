000e9d68c603ae9a51f190b2b621f48756cdd4b1
Alysson Chiarelly <alyssonclr@gmail.com>
2022-08-29 18:45:07 -0300

    Adicionado primeiro exercicio dia 2

---
+++ 01-Fundamentos/Bloco-05-DOM-Eventos-e-Web Storage/Dia-2-Trabalhando-com-Eventos/exercicio-2/script.js
@@ -0,0 +1,11 @@
+console.log(document.getElementById('elementoOndeVoceEsta'));
+console.log(document.getElementById('elementoOndeVoceEsta').parentNode.style.color = 'red');
+document.getElementById('primeiroFilhoDoFilho').innerText = 'Texto adicionado utilizando JavaScript!';
+/*
+Acesse o primeiroFilho a partir de pai.
+Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+Agora acesse o terceiroFilho a partir de pai.
+
+*/
