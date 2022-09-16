f1c7b6b998dd7e08e8fba9cd8a5b6ed68f06b303
Alysson Chiarelly <alyssonclr@gmail.com>
2022-09-16 15:59:10 -0300

    Novos exercicios adicionados

---
+++ 01-Fundamentos/Bloco-07-Introducao-a-JavaScript-ES6-e-Testes-Unitarios/Dia-02-JvavaScript-ES6-Fluxo-de-Execucao-e-Objetos/ObjectEntries/exemplo1.js
@@ -0,0 +1,15 @@
+const coolestTvShow = {
+  name: 'BoJack Horseman',
+  genre: 'adult animation',
+  createdBy: 'Raphael Bob-Waksberg',
+  favoriteCharacter: 'Princess Carolyn',
+  quote: 'Princess Carolyn always lands on her feet.',
+  seasons: 6,
+};
+
+console.log(Object.entries(coolestTvShow));
+
+/* Uma maneira de interagir com objetos é por meio do object.entries, 
+ * ele retorna um array cujos elementos também são arrays, para cada
+ * conjunto chanve e valor do objeto.
+ */
---
+++ 01-Fundamentos/Bloco-07-Introducao-a-JavaScript-ES6-e-Testes-Unitarios/Dia-02-JvavaScript-ES6-Fluxo-de-Execucao-e-Objetos/ObjectEntries/exemplo2.js
@@ -0,0 +1,19 @@
+const países = {
+  França: 'Paris',
+  Brasil: 'Brasília',
+  Espanha: 'Madrid',
+  Portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(países);
+console.log(pairKeyValue);
+
+for(index in pairKeyValue) {
+  console.log('--------');
+  console.log('País:', pairKeyValue[index][0]);
+  console.log('Capital:', pairKeyValue[index][1]);
+};
+
+/* Aqui podemos observer que o retorno é um array e que 
+ * cada um dos seus elementos é outro array com apenas
+ * dois dados: a chave do objeto e o seu valor.
+ */
