3be96346d218f7ad82cd23ea1aefb8bdaa9ebe76
Alysson Chiarelly <alyssonclr@gmail.com>
2022-09-26 14:44:10 -0300

    Adicionado novos exemplos

---
+++ 01-Fundamentos/Secao-08-Higher-Order-Function/Dia-03-Map-e-Filter/exemplo2.js
@@ -0,0 +1,16 @@
+const objPeople = [
+  { name: 'José', age: 21 },
+  { name: 'Lucas', age: 19 },
+  { name: 'Maria', age: 16 },
+  { name: 'Gilberto', age: 18 },
+  { name: 'Vitor', age: 15 },
+];
+
+const verifyAgeDrive = (arrayOfPeople) => (
+  arrayOfPeople.filter((people) => (people.age < 18))
+);
+
+console.log(verifyAgeDrive(objPeople));
+// [ { name: 'Maria', age: 16 }, { name: 'Vitor', age: 15 } ]
+
+//Neste exemplo é retornado as pessoas que tem menos de 18 anos.
